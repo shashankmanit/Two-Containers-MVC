ec02f0d350a8ce67396bdccaf3b561e1e46be854
Shashank-Java <shashankmani02@gmail.com>
2017-04-06 10:37:11 +0530

    added WishServices

--- src/main/java/com/shashank/service/WishService.java
+++ src/main/java/com/shashank/service/WishService.java
@@ -13,13 +13,13 @@ public class WishService {
 		// get system Date and time
 		Calendar calender = Calendar.getInstance();
 		int hour = calender.get(Calendar.HOUR_OF_DAY);
-		String msg = null;
+	 	String msg = null;
 		if (hour < 12)
 			msg = " Good Morning";
 		else if (hour < 16)
-			msg = "Good AfterNoon";
+			msg = " Good AfterNoon";
 		else if (hour < 20)
-			msg = "Good Evening";
+			msg = " Good Evening";
 		else
 			msg = "Good Night";
 
