e25fba7d174b9a68d6bd87d53813a8dfb38e070f
Shashank-Java <shashankmani02@gmail.com>
2018-01-01 10:47:51 +0530

    pom.xml updated

--- src/main/java/com/shashank/controller/WishController.java
+++ src/main/java/com/shashank/controller/WishController.java
@@ -1,16 +1,12 @@
 package com.shashank.controller;
-
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
-
 import com.shashank.service.WishService;
 
 @Controller
@@ -36,5 +32,4 @@ public class WishController extends AbstractController {
 		String wmsg = service.generateWish();
 		return new ModelAndView("result", "wresult", wmsg);
 	}
-
 }
